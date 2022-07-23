fdebe0cb262f715f611faaa7d9a0a0b57c98f2f0
sua bae <bsa0322@ewhain.net>
2022-07-23 22:13:31 +0900

    로그인 작업중

---
+++ login-practice/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const app = express();
+const port = 6000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json);
+
+const moogoose = require("mongoose");
+const { User } = require("./models/User");
+
+mongoose
+  .connect(
+    "mongodb+srv://sua:020322@test.2vd7h.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("connect well!!"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World! ~~ 안녕하세요 ~~");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`example app listening on port ${port}`);
+});
