d22281ea526fd0760eba3aaec768255a904ce370
suabae <bsa0322@ewhain.net>
2022-07-24 01:29:41 +0900

    비밀번호 암호화 + JWT 생성

--- login-practice/index.js
+++ login-practice/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 7000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 const { User } = require("./models/User");
@@ -25,6 +28,42 @@ app.post("/register", (req, res) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+      userInfo,
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  //요청 이메일 DB에서 찾기
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.join({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+
+    // user: 위의 DB에서 찾아온 사용자
+
+    //비밀번호 일치하는지 검사
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+
+      //비밀번호 일치하면 토큰 생성
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 클라이언트의 쿠키에 저장
+        // 저장은 쿠키 / localstorage 등 여러 곳에 저장할 수 있음. 어디에 저장해야 가장 안전한지는 아직까지 말이 많음
+        res.cookie("x_auth", user.token).status(200).json({
+          loginSuccess: true,
+          userId: user._id,
+        });
+      });
     });
   });
 });
--- login-practice/models/User.js
+++ login-practice/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -30,6 +33,47 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        console.log("암호화 완료!");
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, callBack) {
+  //plainPassword를 암호화해서 암호화된 비밀번호와 같은지 확인
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return callBack(err);
+    callBack(null, isMatch);
+  });
+};
+
+// JWT 토큰 생성
+userSchema.methods.generateToken = function (callBack) {
+  const user = this;
+
+  const token = jwt.sign(user._id.toHexString(), "secretToken");
+
+  // user id + secret key -> token (인코드)
+  // secret key + token -> user id (디코드)
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return callBack(err);
+    callBack(null, user);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
